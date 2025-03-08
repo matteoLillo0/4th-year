e643a4775d34eb95061c24f62f3c8930d2382577
matteo.angiolillo <matteo.angiolillo@PC0102BIS>
2025-03-08 10:37:27 +0100

    Added networking project with socket WpfServer

---
+++ WpfServer/WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿// Matteo Angiolillo 4°H 2025-03-08 Client TCP
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net; // da aggiungere manualmente
+using System.Threading; // da aggiungere manualmente
+using System.Net.Sockets;
+using System.CodeDom;
+using System.IO; 
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        private Socket senderSocket;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectToServer(); // connette con l'interfaccia 
+        }
+
+        private void ConnectToServer()
+        {
+            Thread clientThread = new Thread(() => // thread come nel server per non bloccare esecuzione
+            {
+                try
+                {  
+                    // dice indirizzo ip e porta del server
+                    IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                    // Crea la socket
+                    senderSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    senderSocket.Connect(remoteEP);
+
+                    // aggiorna ui con connessione avvenuta
+                    Dispatcher.Invoke(() => txtMessaggiRicevuti.AppendText("Connesso al server.\n"));
+
+                    while (true)
+                    {
+                        byte [] buffer = new byte[1024];
+                        int bytesRec = senderSocket.Receive(buffer); // riceve i dati dal server
+
+                        if (bytesRec == 0) break; // se il server chiude la connessione
+
+                        string response = Encoding.ASCII.GetString(buffer, 0, bytesRec);
+                        Dispatcher.Invoke(() => txtMessaggiRicevuti.AppendText("Server: " + response + "\n"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Dispatcher.Invoke(() => MessageBox.Show("Errore client: " + ex.Message));
+                }
+                finally{
+                    // chiude la connessione in caso di errore 
+                    senderSocket?.Shutdown(SocketShutdown.Both);
+                    senderSocket?.Close();
+                }
+            });
+            clientThread.IsBackground = true; // IMPORTANTE perchè viene terminato quando il thread main finisce 
+            clientThread.Start();
+        }
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (senderSocket!= null && senderSocket.Connected) // controlla la connessione al server
+            {   
+                string message = txtMessaggioInviare.Text;
+                byte[] msgBytes = Encoding.ASCII.GetBytes(message);
+                senderSocket.Send(msgBytes); // invia il messaggio
+
+                txtMessaggiRicevuti.AppendText("(you)Client: " + txtMessaggioInviare.Text + "\n");
+                txtMessaggioInviare.Clear();
+
+            }
+            else
+            {
+                MessageBox.Show("Non connesso al server"); // avvisa se il server non è connesso
+            }
+        }
+    }
+}
---
+++ WpfServer/WpfServer/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿// Matteo Angiolillo 4°H 2025-03-08 Server TCP
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net; // da aggiungere
+using System.Threading; // da aggiungere
+using System.Net.Sockets;
+using System.CodeDom; // da aggiungere
+
+namespace WpfServer
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket listener; // socket che ascolta in ingresso
+        private Socket handler; // socket che gestisce le comunicazioni con il client
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartServer(); // avvia il server con l'interfaccia
+        }
+        private void StartServer()
+        {
+            Thread serverThread = new Thread(() => // thread per non bloccare l'interfaccia e quindi paralelliza server e interfaccia
+            {
+                try
+                {
+                    IPHostEntry host = Dns.GetHostEntry("127.0.0.1"); // ottiene l'ip del server (localhost)
+                    IPAddress ipAddress = host.AddressList[0];
+                    IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+                    // Crea il socket per le connesioni tcp
+                    listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    listener.Bind(localEndPoint);  // primitiva bind che associa il socket alla porta 
+                    listener.Listen(10); // inizia ad ascoltare le connessioni
+
+                    Dispatcher.Invoke(() => txtMessaggiRicevuti.AppendText("Server in ascolto...\n")); // aggiorna l'ui
+
+                    // accetta una conessione dal client
+                    handler = listener.Accept();
+                    Dispatcher.Invoke(() => txtMessaggiRicevuti.AppendText("Client connesso.\n")); 
+
+                    while (true) 
+                    {
+                        byte[] buffer = new byte[1024];
+                        int bytesRec = handler.Receive(buffer); // primitiva Receive che riceve i dati dal client
+                        if (bytesRec == 0) break;
+
+                        string data = Encoding.ASCII.GetString(buffer, 0, bytesRec); 
+                        Dispatcher.Invoke(() => txtMessaggiRicevuti.AppendText("Client: " + data + "\n")); // aggiorna l'ui con i messaggi del client
+
+                        // Risponde al client confermando che ha ricevuto
+                        byte[] response = Encoding.ASCII.GetBytes("Ricevuto: " + data); 
+                        handler.Send(response); // primitiva send invia la risposta
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Dispatcher.Invoke(() => MessageBox.Show("Errore server: " + e.Message));
+                }
+                finally
+                {   // chiude il socket se ci sono errori
+                    handler?.Shutdown(SocketShutdown.Both); // nullable
+                    handler?.Close();
+                    listener?.Close();
+                }
+            });            
+
+            serverThread.IsBackground = true; // IMPORTANTE perchè viene terminato quando il thread main finisce 
+            serverThread.Start();
+        }
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (handler != null && handler.Connected) // verifica il client sia connesso
+            {
+                string message = txtMessaggioInviare.Text; // legge il messaggio da inviare
+                byte[] msgBytes = Encoding.ASCII.GetBytes(message); // lo converte in byte
+                handler.Send(msgBytes); // primitiva send e lo manda
+
+                txtMessaggiRicevuti.AppendText("(you)Server: " + message + "\n");  // lo aggiunge alla casella dei messaggi ricevuti
+                txtMessaggioInviare.Clear(); // pulisce la casella di input
+
+            }
+            else
+            {
+                MessageBox.Show("nessun client connesso"); // messaggio di errore 
+
+            }
+        }
+    }
+}
